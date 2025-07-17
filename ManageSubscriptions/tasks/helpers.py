b46c39a40845047f3fed74764fba9bf9910069e0
Mohammad Mhaidat <mojamusa05@gmail.com>
2025-07-17 18:00:57 +0300

    Update helpers.py

--- ManageSubscriptions/tasks/helpers.py
+++ ManageSubscriptions/tasks/helpers.py
@@ -3,7 +3,7 @@ from ManageSubscriptions import mail
 
 def send_logs(subject,user_email,body,attachment_file):
     msg = Message(subject, 
-                sender='buisnesscybergpt05@gmail.com', 
+                sender='sublyio@gmail.com', 
                 recipients=[user_email])
     msg.body = body
     with open(attachment_file, 'rb') as file:
